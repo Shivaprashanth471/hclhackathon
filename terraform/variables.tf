1dd54505f038f5dc02f9ebd44feb8b0aa01926fb
Shiva Prashanth <92975325+Shivaprashanth471@users.noreply.github.com>
2025-11-24 10:30:12 +0530

    Ci: add terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+  default = "flash-spot-478811-m2"
+}
+variable "region" {
+  default = "us-central1"
+}
+variable "zone" {
+  default = "us-central1-a"
+}
+variable "credentials_json" {
+  sensitive = true
+}
+
+variable "GOOGLE_CREDENTIALS" {
+  type = string
+}
